e014d799dba42b0c4d2ea53b5c08644289636f28
Iain Lane <iain.lane@canonical.com>
2021-01-08 17:57:53 +0000

    autopkgtest: Fetch staging data too
    Add an 'instance' tag to differentiate.

--- metrics/collectors/autopkgtest/autopkgtest_collector.py
+++ metrics/collectors/autopkgtest/autopkgtest_collector.py
@@ -9,8 +9,15 @@ from distro_info import UbuntuDistroInfo as UDI
 from metrics.lib.basemetric import Metric
 from metrics.lib.ubunturelease import UbuntuRelease
 
-QUEUE_SIZE_URL = "https://autopkgtest.ubuntu.com/queue_size.json"
-RUNNING_URL = "https://autopkgtest.ubuntu.com/static/running.json"
+QUEUE_SIZE_URL = {
+    "production": "https://autopkgtest.ubuntu.com/queue_size.json",
+    "staging": "https://autopkgtest.staging.ubuntu.com/queue_size.json",
+}
+
+RUNNING_URL = {
+    "production": "https://autopkgtest.ubuntu.com/static/running.json",
+    "staging": "https://autopkgtest.staging.ubuntu.com/static/running.json",
+}
 
 
 class AutopkgtestMetrics(Metric):
@@ -20,45 +27,52 @@ class AutopkgtestMetrics(Metric):
 
     def collect_queue_sizes(self):
         data = []
-        queue_sizes = self.fetch(QUEUE_SIZE_URL)
-        for context in queue_sizes:
-            for release in queue_sizes[context]:
-                for arch, count in queue_sizes[context][release].items():
-                    data.append(
-                        {
-                            "measurement": "autopkgtest_queue_size",
-                            "fields": {"count": count},
-                            "tags": {
-                                "context": context,
-                                "release": release,
-                                "arch": arch,
-                            },
-                        }
-                    )
+        for instance in ("production", "staging"):
+            queue_sizes = self.fetch(QUEUE_SIZE_URL[instance])
+            for context in queue_sizes:
+                for release in queue_sizes[context]:
+                    for arch, count in queue_sizes[context][release].items():
+                        data.append(
+                            {
+                                "measurement": "autopkgtest_queue_size",
+                                "fields": {"count": count},
+                                "tags": {
+                                    "context": context,
+                                    "release": release,
+                                    "arch": arch,
+                                    "instance": instance,
+                                },
+                            }
+                        )
         return data
 
     def collect_running(self):
         data = []
-        running = self.fetch(RUNNING_URL)
-        counts = defaultdict(lambda: defaultdict(int))
+        for instance in ("production", "staging"):
+            running = self.fetch(RUNNING_URL[instance])
+            counts = defaultdict(lambda: defaultdict(int))
 
-        for pkg in running:
-            for params in running[pkg]:
-                for release in running[pkg][params]:
-                    for arch in running[pkg][params][release]:
-                        counts[release][arch] += 1
+            for pkg in running:
+                for params in running[pkg]:
+                    for release in running[pkg][params]:
+                        for arch in running[pkg][params][release]:
+                            counts[release][arch] += 1
 
-        for release in counts:
-            for arch, count in counts[release].items():
-                data.append(
-                    {
-                        "measurement": "autopkgtest_running",
-                        "fields": {"count": count},
-                        "tags": {"release": release, "arch": arch},
-                    }
-                )
+            for release in counts:
+                for arch, count in counts[release].items():
+                    data.append(
+                        {
+                            "measurement": "autopkgtest_running",
+                            "fields": {"count": count},
+                            "tags": {
+                                "release": release,
+                                "arch": arch,
+                                "instance": instance,
+                            },
+                        }
+                    )
 
-        return data
+            return data
 
     def collect(self):
         d1 = self.collect_queue_sizes()
