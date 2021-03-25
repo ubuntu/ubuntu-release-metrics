464ba39f8586200b688185c307daaafe1035fdd6
Sebastien Bacher <seb128@ubuntu.com>
2021-03-25 11:07:15 +0100

    Tweaks to address the review comments

--- metrics/collectors/images/images_collector.py
+++ metrics/collectors/images/images_collector.py
@@ -30,11 +30,12 @@ class ImagesMetrics(Metric):
         self.active_series = {s.name: s for s in self.ubuntu.series if s.active}
         self.date_now = datetime.datetime.now()
 
-    def rsync_lists_images(self):
+    def rsync_list_images(self):
         rsync = ""
         for url in RSYNC_SERVER_REQUESTS:
             for img in IMAGE_FORMATS:
                 try:
+                    self.log.debug("Rsync listing %s" % url + img)
                     rsync += subprocess.check_output(
                         [
                             "rsync",
@@ -49,64 +50,62 @@ class ImagesMetrics(Metric):
                         text=True,
                     )
                 except subprocess.CalledProcessError as e:
-                    print("Rsync call failed: ", e.output)
+                    self.log.error("rsync call failed: %s", e.output)
         return rsync
 
     def collect(self):
         """ Collect the daily images details"""
         data = []
 
-        rsync_cmd_output = self.rsync_lists_images()
+        rsync_cmd_output = self.rsync_list_images()
 
         for rsyncline in rsync_cmd_output.splitlines():
-            if any(ext in rsyncline for ext in IMAGE_FORMATS):
-                if "current" in rsyncline or "pending" in rsyncline:
-                    # the format is specific in the rsync call '%l %M %f'
-                    size, mtime, path = rsyncline.strip("'").split()
-                    date_current_image = datetime.datetime.strptime(
-                        mtime, "%Y/%m/%d-%H:%M:%S"
-                    )
-                    image_age = (self.date_now - date_current_image).days
+            if not any(ext in rsyncline for ext in IMAGE_FORMATS):
+                continue
+            if "current" in rsyncline or "pending" in rsyncline:
+                # the format is specific in the rsync call '%l %M %f'
+                size, mtime, path = rsyncline.strip("'").split()
+                date_current_image = datetime.datetime.strptime(
+                    mtime, "%Y/%m/%d-%H:%M:%S"
+                )
+                image_age = (self.date_now - date_current_image).days
 
-                    # there are variations of the naming scheme so guess a bit
-                    path_table = path.split("/")
-                    # the path starts with a serie name then it's a desktop iso
-                    if path_table[0] in self.active_series:
-                        flavor = "ubuntu"
-                    # or with 'daily-live'
-                    elif path_table[0] == "daily-live":
-                        flavor = "ubuntu"
-                    # otherwise it starts with the flavor
-                    else:
-                        flavor = path_table[0]
+                # there are variations of the naming scheme so guess a bit
+                path_table = path.split("/")
+                # the path starts with a serie name then it's a desktop iso
+                if path_table[0] in self.active_series:
+                    flavor = "ubuntu"
+                # or with 'daily-live'
+                elif path_table[0] == "daily-live":
+                    flavor = "ubuntu"
+                # otherwise it starts with the flavor
+                else:
+                    flavor = path_table[0]
 
-                    # the path ends with the image filename
-                    image_name = path_table[-1]
+                # the path ends with the image filename
+                image_name = path_table[-1]
 
-                    # let's filter out old ubuntu-core-16 images
-                    if image_name.startswith("ubuntu-core-16"):
-                        continue
+                # let's filter out old ubuntu-core-16 images
+                if image_name.startswith("ubuntu-core-16"):
+                    continue
 
-                    if "current" in rsyncline:
-                        daily_or_current = "current"
-                    else:
-                        daily_or_current = "daily"
+                if "current" in rsyncline:
+                    current_or_pending = "current"
+                else:
+                    current_or_pending = "daily"
 
-                    regexp = re.compile(r"^(\w+)-.*-([\w\+]+)\..+")
-                    series, arch = regexp.search(image_name).groups()
+                regexp = re.compile(r"^(\w+)-.*-([\w\+]+)\..+")
+                series, arch = regexp.search(image_name).groups()
 
-                    data.append(
-                        {
-                            "measurement": "daily_images_details",
-                            "fields": {
-                                "age": image_age,
-                                "size": size,
-                            },
-                            "tags": {
-                                "flavor": flavor,
-                                "release": series,
-                                "daily_or_current": daily_or_current,
-                            },
-                        }
-                    )
+                data.append(
+                    {
+                        "measurement": "daily_image_details",
+                        "fields": {"age": image_age, "size": size},
+                        "tags": {
+                            "flavor": flavor,
+                            "release": series,
+                            "current_or_pending": current_or_pending,
+                        },
+                    }
+                )
         return data
