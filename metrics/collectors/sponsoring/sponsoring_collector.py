d4ebd353bc3051b7b778cb58c77578088042d8a2
Sebastien Bacher <seb128@ubuntu.com>
2021-04-06 16:51:30 +0200

    Report the count of the different sponsoring categories

--- metrics/collectors/sponsoring/sponsoring_collector.py
+++ metrics/collectors/sponsoring/sponsoring_collector.py
@@ -1,27 +1,37 @@
 # Copyright 2021 Canonical Ltd
 
 import json
+import re
 import urllib.request
 
 from metrics.lib.basemetric import Metric
 
-SPONSORING_QUEUE_URL = "http://reqorts.qa.ubuntu.com/reports/sponsoring/sponsoring.json"
+SPONSORING_QUEUE_DIR = "http://reqorts.qa.ubuntu.com/reports/sponsoring/jsons/"
 
 
 class SponsoringMetrics(Metric):
     def collect(self):
         """ Collect the sponsoring queue details"""
         data = []
+        reports = []
 
-        with urllib.request.urlopen(SPONSORING_QUEUE_URL) as url:
-            report = json.load(url)
+        with urllib.request.urlopen(SPONSORING_QUEUE_DIR) as url:
+            for index in url.read().decode("utf-8", errors="ignore").split("\n"):
+                if ".json" in index:
+                    r = re.compile(r'<a href="([\w.-]+)">')
+                    reports.append(r.search(index).group(1))
 
-            data.append(
-                {
-                    "measurement": "sponsoring_queue_stats",
-                    "fields": {"count": len(report)},
-                    "tags": {"report": "ubuntu"},
-                }
-            )
+        for report in reports:
+            reporturl = "%s%s" % (SPONSORING_QUEUE_DIR, report)
+            with urllib.request.urlopen(reporturl) as url:
+                reportcontent = json.load(url)
+
+                data.append(
+                    {
+                        "measurement": "sponsoring_queue_stats",
+                        "fields": {"count": len(reportcontent)},
+                        "tags": {"report": report.replace(".json", "")},
+                    }
+                )
 
         return data
