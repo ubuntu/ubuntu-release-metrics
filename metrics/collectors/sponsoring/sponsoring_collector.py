4f57472710d920c82e060c81aeeb947819cde713
Sebastien Bacher <seb128@ubuntu.com>
2021-04-06 17:34:14 +0200

    include oldest report and average age in the queue

--- metrics/collectors/sponsoring/sponsoring_collector.py
+++ metrics/collectors/sponsoring/sponsoring_collector.py
@@ -1,5 +1,6 @@
 # Copyright 2021 Canonical Ltd
 
+import datetime
 import json
 import re
 import urllib.request
@@ -14,6 +15,7 @@ class SponsoringMetrics(Metric):
         """ Collect the sponsoring queue details"""
         data = []
         reports = []
+        datenow = datetime.datetime.now()
 
         with urllib.request.urlopen(SPONSORING_QUEUE_DIR) as url:
             for index in url.read().decode("utf-8", errors="ignore").split("\n"):
@@ -22,14 +24,29 @@ class SponsoringMetrics(Metric):
                     reports.append(r.search(index).group(1))
 
         for report in reports:
+            age_of_items = []
             reporturl = "%s%s" % (SPONSORING_QUEUE_DIR, report)
             with urllib.request.urlopen(reporturl) as url:
                 reportcontent = json.load(url)
 
+                for entry in reportcontent:
+                    date_queue = datetime.datetime.strptime(
+                        entry["date_queued"], "%m/%d/%y"
+                    )
+                    item_age = (datenow - date_queue).days
+                    age_of_items.append(item_age)
+
+                oldest = max(age_of_items)
+                average_age = int(sum(age_of_items) / len(age_of_items))
+
                 data.append(
                     {
                         "measurement": "sponsoring_queue_stats",
-                        "fields": {"count": len(reportcontent)},
+                        "fields": {
+                            "count": len(reportcontent),
+                            "oldest": oldest,
+                            "average_age": average_age,
+                        },
                         "tags": {"report": report.replace(".json", "")},
                     }
                 )
