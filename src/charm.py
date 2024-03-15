c1e5fe63831946b9c78fc1b7e3cc39dde268c9c7
Tim Andersson <tim.andersson@canonical.com>
2024-03-15 16:07:21 +0000

    fix: clone repo as ubuntu user under .local instead of /srv/
    This commit also amends all the service files to expect all of the
    scripts etc to be under this new .local location.
    
    The environment files required for the various metrics collector
    services to run have also now been placed under
    /home/ubuntu/.local/share, like the repository.

--- src/charm.py
+++ src/charm.py
@@ -13,10 +13,11 @@ from ops.main import main
 
 logger = logging.getLogger(__name__)
 
-CREDENTIALS_FILE = "/srv/influx.conf"
-DRY_RUN_FILE = "/srv/dry-run.conf"
-ENVIRONMENT_FILE = "/etc/environment.d/proxy.conf"
+CREDENTIALS_FILE = "/home/ubuntu/.local/share/influx.conf"
+DRY_RUN_FILE = "/home/ubuntu/.local/share/dry-run.conf"
+ENVIRONMENT_FILE = "/home/ubuntu/.local/share/proxy.conf"
 METRICS_REPO = "https://github.com/ubuntu/ubuntu-release-metrics.git"
+CLONE_LOCATION = "/home/ubuntu/.local/share/ubuntu-release-metrics"
 PACKAGES_TO_INSTALL = ["git", "python3-influxdb", "python3-launchpadlib"]
 
 
@@ -66,10 +67,14 @@ class UbuntuReleaseMetricsCollectorCharm(CharmBase):
         logger.info(f"Cloning {METRICS_REPO}")
         subprocess.check_call(
             [
+                "sudo",
+                "-H",
+                "-u",
+                "ubuntu",
                 "git",
                 "clone",
                 METRICS_REPO,
-                os.path.expanduser("/srv/ubuntu-release-metrics/"),
+                CLONE_LOCATION,
             ],
             env=self.subprocess_env,
         )
@@ -79,6 +84,7 @@ class UbuntuReleaseMetricsCollectorCharm(CharmBase):
         self._install_packages(set(PACKAGES_TO_INSTALL))
         self._clone_metrics_repo()
         unit_dir = "/etc/systemd/system"
+
         for unit in (
             "daemon-reload.service",
             "daemon-reload.timer",
