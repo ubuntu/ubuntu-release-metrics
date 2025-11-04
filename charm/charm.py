2d6be55344f61e893b484b0a04f2266a890b530a
Tim Andersson <tim.andersson@canonical.com>
2025-11-04 12:25:10 +0000

    add ops charm
    This charm is pretty simple - it just sets up the systemd units for the
    ubuntu-release-metrics based on a given juju config.

---
+++ charm/charm.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# Copyright 2025 ssstr1pe
+# See LICENSE file for licensing details.
+
+"""Charm the release metrics."""
+
+import logging
+
+import ops
+
+from releasemetrics import ReleaseMetrics
+
+class ReleaseMetricsCharm(ops.CharmBase):
+    """Charm the release metrics"""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self._release_metrics = ReleaseMetrics()
+
+        self.framework.observe(self.on.start, self._on_start)
+        self.framework.observe(self.on.install, self._on_install)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+
+    def _on_start(self, event: ops.StartEvent):
+        """Handle start event."""
+        self.unit.status = ops.ActiveStatus()
+
+    def _on_install(self, event: ops.InstallEvent):
+        """Handle install event."""
+        self.unit.status = ops.MaintenanceStatus("installing ubuntu-release-metrics metric collectors")
+        try:
+            self._release_metrics.install(self.config)
+        except Exception as e:
+            logger.error("failed to install ubuntu-release-metrics: %s", str(e))
+            self.unit.status = ops.BlockedStatus("failed installing ubuntu-release-metrics")
+            return
+
+        self.unit.status = ops.ActiveStatus("Ready")
+
+    def _on_config_changed(self, event: ops.ConfigChangedEvent):
+        self.unit.status = ops.MaintenanceStatus("ubuntu-release-metrics charm configuration updated - updating unit")
+        self._release_metrics.configure(self.config)
+        self.unit.status = ops.ActiveStatus("ready")
