1d1884b9f193faa1258b030cfcf12f73afb6e23c
Florent 'Skia' Jacquet <skia@ubuntu.com>
2026-04-03 17:46:19 +0200

    charm: simplify charm by always self-hosting InfluxDB and Grafana

--- charm/charm.py
+++ charm/charm.py
@@ -30,59 +30,54 @@ class UbuntuReleaseMetricsCharm(ops.CharmBase):
 
     def _on_install(self, event: ops.InstallEvent):
         """Handle install event."""
-        self.unit.status = ops.MaintenanceStatus(
-            "installing ubuntu-release-metrics metric collectors"
-        )
-        if not self.config.get("influxdb_hostname"):
-            # Self-host InfluxDB and Grafana
-            try:
-                self._influxdb.install()
-                self._grafana.install()
-            except Exception as e:
-                self.unit.status = ops.BlockedStatus(
-                    f"failed installing influxdb/grafana: {e}"
-                )
-                raise e
+        self.unit.status = ops.MaintenanceStatus("installing ubuntu-release-metrics")
+        try:
+            self._influxdb.install()
+        except Exception as e:
+            self.unit.status = ops.BlockedStatus(f"failed installing influxdb: {e}")
+            raise e
+        try:
+            self._grafana.install()
+        except Exception as e:
+            self.unit.status = ops.BlockedStatus(f"failed installing grafana: {e}")
+            raise e
         try:
             self._release_metrics.install()
         except Exception as e:
-            self.unit.status = ops.BlockedStatus(
-                f"failed installing ubuntu-release-metrics: {e}"
-            )
+            self.unit.status = ops.BlockedStatus(f"failed installing collector: {e}")
             raise e
 
         self.unit.status = ops.ActiveStatus("Ready")
 
     def _on_config_changed(self, event: ops.ConfigChangedEvent):
-        self.unit.status = ops.MaintenanceStatus(
-            "ubuntu-release-metrics charm configuration updated - updating unit"
-        )
+        self.unit.status = ops.MaintenanceStatus("rolling out configuration")
+
         influxdb_config = {}
-        if not self.config.get("influxdb_hostname"):
-            # Self-host InfluxDB and Grafana
-            try:
-                self._influxdb.configure(self.config)
-                influxdb_config["influxdb_hostname"] = self._influxdb.influxdb_hostname
-                influxdb_config["influxdb_port"] = self._influxdb.influxdb_port
-                influxdb_config["influxdb_username"] = self._influxdb.influxdb_username
-                influxdb_config["influxdb_password"] = self._influxdb.influxdb_password
-                influxdb_config["influxdb_database"] = self._influxdb.influxdb_database
-                self._grafana.configure(self.config)
-                self.unit.set_ports(
-                    self._influxdb.influxdb_port, self._grafana.grafana_port
-                )
-            except Exception as e:
-                self.unit.status = ops.BlockedStatus(
-                    f"failed configuring influxdb: {e}"
-                )
-                raise e
+
+        try:
+            self._influxdb.configure(self.config)
+            influxdb_config["influxdb_hostname"] = self._influxdb.influxdb_hostname
+            influxdb_config["influxdb_port"] = self._influxdb.influxdb_port
+            influxdb_config["influxdb_username"] = self._influxdb.influxdb_username
+            influxdb_config["influxdb_password"] = self._influxdb.influxdb_password
+            influxdb_config["influxdb_database"] = self._influxdb.influxdb_database
+        except Exception as e:
+            self.unit.status = ops.BlockedStatus(f"failed configuring influxdb: {e}")
+            raise e
+
+        try:
+            self._grafana.configure(self.config)
+        except Exception as e:
+            self.unit.status = ops.BlockedStatus(f"failed configuring grafana: {e}")
+            raise e
+
         try:
             self._release_metrics.configure({**self.config, **influxdb_config})
         except Exception as e:
-            self.unit.status = ops.BlockedStatus(
-                f"failed configuring ubuntu-release-metrics: {e}"
-            )
+            self.unit.status = ops.BlockedStatus(f"failed configuring collector: {e}")
             raise e
+
+        self.unit.set_ports(self._influxdb.influxdb_port, self._grafana.grafana_port)
         self.unit.status = ops.ActiveStatus("ready")
 
 
